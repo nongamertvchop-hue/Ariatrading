09a2118ced4af96bafd2e3049ff012b9e2b845cf
nongamertvchop-hue <nongamertvchop@gmail.com>
2026-09-07 09:37:24 +0700

    Export portfolio and hard trade guard APIs

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -29,6 +29,7 @@ from .ml_walk_forward import MLWalkForwardFold, MLWalkForwardResult, ml_walk_for
 from .paper import CLOSED, OPEN, PaperAccount, PaperPosition, PaperTradingEngine
 from .paper_session import PaperSessionResult, PaperSessionRunner
 from .pipeline import ResearchReport, run_research
+from .portfolio_risk import ALLOW as PORTFOLIO_ALLOW, HALT, PortfolioRiskController, PortfolioRiskDecision, PortfolioRiskLimits, PortfolioRiskState
 from .realtime_guard import DataQuality, RealtimeGuard, expected_closed_bar_open
 from .realtime_replay import RealtimeReplayResult, replay_realtime_monitor
 from .realtime_supervisor import ALLOW, SupervisorDecision, supervise
@@ -47,6 +48,7 @@ from .research_runner import ControlledResearchResult, run_controlled_research
 from .research_validation import ValidationEvidence, build_validation_evidence
 from .regime import REGIMES, RegimeStats, classify_regime, stratify_samples
 from .risk_engine import RiskDecision, RiskLimits, evaluate_risk, position_size
+from .trade_guard import TradeGuardDecision, evaluate_trade_guard
 from .two_setups import TwoSetupResult, evaluate_two_setups
 from .walk_forward import WalkForwardFold, WalkForwardResult, walk_forward_backtest
 
@@ -54,6 +56,9 @@ __all__ = [
     "EngineSignal", "LONG", "SHORT", "WAIT", "evaluate_long", "evaluate_short",
     "TwoSetupResult", "evaluate_two_setups",
     "RiskLimits", "RiskDecision", "position_size", "evaluate_risk",
+    "PortfolioRiskLimits", "PortfolioRiskState", "PortfolioRiskDecision",
+    "PortfolioRiskController", "PORTFOLIO_ALLOW", "HALT",
+    "TradeGuardDecision", "evaluate_trade_guard",
     "ExecutionModel", "entry_price", "exit_price",
     "ExperimentRecord", "build_experiment_record",
     "ForecastResult", "HorizonForecast", "ScenarioForecast", "forecast",
@@ -74,7 +79,7 @@ __all__ = [
     "INCOMPLETE", "READY", "ResearchGateDecision", "evaluate_research_gate",
     "ResearchAuditReport", "audit_validation_evidence",
     "FEATURE_NAMES", "MLSample", "build_signal_sample", "extract_signal_features",
-    "MLResearchMetrics", "MetaFilterModel", "MetaFilterResult", "chronological_train_test",
+    "MLResearchMetrics", "MetaFilterModel", "chronological_train_test",
     "WalkForwardFold", "WalkForwardResult", "walk_forward_backtest",
     "MLWalkForwardFold", "MLWalkForwardResult", "ml_walk_forward_backtest",
     "MLComparison", "compare_ml_results",
